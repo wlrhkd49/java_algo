4b09b590ce17b170cd8235a5ea13c5224db730da
JiKwang <wlrhkd49@naver.com>
2022-04-06 10:32:14 +0900

    국영수 정렬 복습

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,58 +1,51 @@
 package test;
 
-import javax.lang.model.element.Element;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scan = new Scanner(System.in);
     static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] A;
+    static int N;
+    static Elem[] A;
 
-    static void input() {
-        N = scan.nextInt();
-        M = scan.nextInt();
-        A = new int[N+1];
-        for(int i = 1; i<=N; i++) {
-            A[i] = scan.nextInt();
+    static public class Elem implements Comparable<Elem> {
+        String name;
+        int kor, eng, math;
+
+        @Override
+        public int compareTo(Elem o) {
+            // 내림차순
+            if(kor != o.kor) return o.kor - kor;
+            if(eng != o.eng) return eng - o.eng;
+            if(math != o.math) return o.math - math;
+            return name.compareTo(o.name);
         }
     }
 
-    static boolean determination(int H) {
-        int sum = 0;
-        for(int i = 1; i <= N; i++) {
-            if(A[i] - H > 0) {
-                sum += A[i] - H;
-            }
-        }
-
-        if(sum >= M) {
-            return true;
-        }
-        else {
-            return false;
+    static void input() {
+        N = scan.nextInt();
+        A = new Elem[N+1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = new Elem();
+            A[i].name = scan.next();
+            A[i].kor = scan.nextInt();
+            A[i].eng = scan.nextInt();
+            A[i].math = scan.nextInt();
         }
     }
 
     static void pro() {
-        int L = 0, R = 2000000000, result = 0;
-        while(L<=R) {
-            int mid = (L+R)/2;
-            if(determination(mid)) {
-                result = mid;
-                L = mid + 1;
-            }
-            else {
-                R = mid - 1;
-            }
+        Arrays.sort(A, 1, N+1);
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(A[i].name).append('\n');
         }
-        System.out.println(result);
     }
 
     public static void main(String[] args) {
         input();
         pro();
+        System.out.println(sb);
     }
 }
