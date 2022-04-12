31d37ea6fb3143ce17fca88345b3356bd3dc6995
JiKwang <wlrhkd49@naver.com>
2022-04-12 13:11:39 +0900

    연구소 복습

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,117 +7,100 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-<<<<<<< HEAD
-    static ArrayList<Integer> group;
-    static int N;
+    static int N, M, B, ans;
     static boolean[][] visit;
-    static String[] a;
-=======
-    static int N, group_cnt;
-    static ArrayList<Integer> group;
-    static String[] a;
-    static boolean[][] visit;
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+    static int[][] a, blank;
     static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     static void input() {
         N = scan.nextInt();
-<<<<<<< HEAD
-        visit = new boolean[N][N];
-        a = new String[N];
-        group = new ArrayList<>();
-=======
-        a = new String[N];
-        group = new ArrayList<>();
-        visit = new boolean[N][N];
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+        M = scan.nextInt();
+        a = new int[N][M];
+        blank = new int[N * M + 1][2];
+        visit = new boolean[N][M];
+        ans = Integer.MIN_VALUE;
 
         for(int i = 0; i < N; i++) {
-            a[i] = scan.next();
+            for(int j = 0; j < M; j++) {
+                a[i][j] = scan.nextInt();
+            }
         }
     }
 
-<<<<<<< HEAD
-    static void bfs(int x, int y) {
-=======
-    static void BFS(int x, int y) {
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+    // 바이러스 전파
+    static void bfs() {
         Queue<Integer> Q = new LinkedList<>();
 
-        Q.add(x);
-        Q.add(y);
-        visit[x][y] = true;
-<<<<<<< HEAD
-        int cnt = 0;
-=======
-        group_cnt++;
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                visit[i][j] = false;
+                if(a[i][j] == 2) {
+                    Q.add(i);
+                    Q.add(j);
+                    visit[i][j] = true;
+                }
+            }
+        }
 
         while(!Q.isEmpty()) {
-            x = Q.poll();
-            y = Q.poll();
-<<<<<<< HEAD
-            cnt++;
-=======
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+            int x = Q.poll();
+            int y = Q.poll();
 
             for(int k = 0; k < 4; k++) {
                 int nx = x + dir[k][0];
                 int ny = y + dir[k][1];
 
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(a[nx][ny] != 0) continue;
                 if(visit[nx][ny]) continue;
-<<<<<<< HEAD
-                if(a[nx].charAt(ny) != '1') continue;
-=======
-                if(a[nx].charAt(ny) == '0') continue;
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
 
                 Q.add(nx);
                 Q.add(ny);
                 visit[nx][ny] = true;
-<<<<<<< HEAD
             }
         }
-        group.add(cnt);
-=======
-                group_cnt++;
-            }
-        }
-        group.add(group_cnt);
 
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+        // 탐색 종료후 안전 개수 찾기
+        int cnt = 0;
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if(a[i][j] == 0 && !visit[i][j]) {
+                    cnt++;
+                }
+            }
+        }
+        ans = Math.max(ans, cnt);
+    }
+
+    // 벽 3개 세우기
+    static void dfs(int idx, int selected_cnt) {
+        if(selected_cnt == 3) {
+            bfs();
+            return;
+        }
+        if(idx > B) return;
+
+        // 벽 세움
+        a[blank[idx][0]][blank[idx][1]] = 1;
+        dfs(idx + 1, selected_cnt + 1);
+
+        a[blank[idx][0]][blank[idx][1]] = 0;
+        dfs(idx + 1, selected_cnt);
     }
 
     static void pro() {
         for(int i = 0; i < N; i++) {
-<<<<<<< HEAD
-            for(int j = 0; j < N; j++) {
-                if(!visit[i][j] && a[i].charAt(j) == '1') {
-                    bfs(i,j);
-=======
-            for (int j = 0; j < N; j ++) {
-                if(!visit[i][j] && a[i].charAt(j) == '1') {
-                    group_cnt = 0;
-                    BFS(i, j);
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
+            for(int j = 0; j < M; j++) {
+                if(a[i][j] == 0) {
+                    B++;
+                    blank[B][0] = i;
+                    blank[B][1] = j;
                 }
             }
         }
 
-<<<<<<< HEAD
-        sb.append(group.size()).append('\n');
-        Collections.sort(group);
-        for(int x : group) sb.append(x).append('\n');
-=======
-        // 출력결과 정렬하기
-        sb.append(group.size()).append('\n');
-        Collections.sort(group);
-        for(int i = 0; i < group.size(); i++){
-            sb.append(group.get(i)).append('\n');
-        }
->>>>>>> 3b4acf12775f58fd5525c5528216349549d43f3e
-        System.out.println(sb);
+        dfs(1, 0);
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
