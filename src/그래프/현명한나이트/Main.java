084fe154a877eba4cce409216c772b7b4507263e
JiKwang <wlrhkd49@naver.com>
2022-04-26 13:42:09 +0900

    [그래프] 현명한 나이트

--- src/그래프/현명한나이트/Main.java
+++ src/그래프/현명한나이트/Main.java
@@ -1,57 +1,62 @@
-package test;
+package 그래프.현명한나이트;
 
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    static int N, start, dest, M;
-    static ArrayList<Integer>[] adj;
-    static boolean[] visit;
-    static int[] dist;
+    static int[][] dir = {{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
+    static int N, M, X, Y;
+    static int[][] dist;
+    static boolean[][] visit;
 
     static void input() {
         N = scan.nextInt();
-        start = scan.nextInt();
-        dest = scan.nextInt();
         M = scan.nextInt();
-        adj = new ArrayList[N+1];
-        for(int i = 1; i <= N; i++) {
-            adj[i] = new ArrayList<>();
-        }
-        for(int i = 1; i <= M; i++) {
-            int x = scan.nextInt(), y = scan.nextInt();
-            adj[x].add(y);
-            adj[y].add(x);
-        }
-        dist = new int[N+1];
-        visit = new boolean[N+1];
+        X = scan.nextInt();
+        Y = scan.nextInt();
+        dist = new int[N+1][N+1];
+        visit = new boolean[N+1][N+1];
     }
 
-    static void bfs(int x) {
+    static void bfs(int x, int y) {
         Queue<Integer> Q = new LinkedList<>();
 
         Q.add(x);
-        visit[x] = true;
+        Q.add(y);
+        visit[x][y] = true;
 
         while(!Q.isEmpty()) {
             x = Q.poll();
+            y = Q.poll();
 
-            for(int y : adj[x]) {
-                if(visit[y]) continue;
+            for(int k = 0; k < 8; k++) {
+                int nx = x + dir[k][0];
+                int ny = y + dir[k][1];
 
-                Q.add(y);
-                visit[y] = true;
-                dist[y] = dist[x] + 1;
+                if(nx < 1 || ny < 1 || nx > N || ny > N) continue;
+                if(visit[nx][ny]) continue;
+
+                Q.add(nx);
+                Q.add(ny);
+                visit[nx][ny] = true;
+                dist[nx][ny] = dist[x][y] + 1;
             }
         }
+
     }
 
     static void pro() {
-        bfs(start);
-        if(dist[dest] == 0) System.out.println(-1);
-        else System.out.println(dist[dest]);
+        bfs(X, Y);
+        for(int i = 1; i <= M; i++) {
+            int x = scan.nextInt(), y = scan.nextInt();
+            sb.append(dist[x][y]).append(' ');
+        }
+        System.out.println(sb);
     }
 
     public static void main(String[] args) {
