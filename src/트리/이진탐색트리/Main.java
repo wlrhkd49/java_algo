94738d520d0a2207fe2a41e29fb32cd306ab95f1
JiKwang <wlrhkd49@naver.com>
2022-04-30 14:30:23 +0900

    [트리] 이진탐색트리

--- src/트리/이진탐색트리/Main.java
+++ src/트리/이진탐색트리/Main.java
@@ -14,21 +14,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = "";
         a = new ArrayList<>();
-        while((input = br.readLine()) != null) {
+        while ((input = br.readLine()) != null){
             a.add(Integer.parseInt(input));
         }
     }
 
+    // 현재 Subtree가 a[l...r] 일 때, 후위 순회를 하는 함수.
     static void traverse(int l, int r) {
-        if(l > r) return;
-        int mid = r;
-        for(int i = l + 1; i <= r; i++) {
-            if(a.get(i) > a.get(l)) {
+        if (l > r) return;
+        int mid = r;  // 왼쪽과 오른쪽 subtree를 가르는 기준 위치를 나타내는 변수
+        for (int i = l + 1; i <= r; i++) {
+            if (a.get(i) > a.get(l)) {
                 mid = i - 1;
                 break;
             }
         }
 
+        // 후위 순회
         traverse(l + 1, mid);
         traverse(mid + 1, r);
         sb.append(a.get(l)).append('\n');
